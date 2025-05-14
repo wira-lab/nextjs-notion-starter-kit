849786ffabadb3b07ded3cf20c28c2cf64865273
wira-lab <wirahhtg@gmail.com>
2025-05-14 20:35:13 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -3,7 +3,7 @@ export default {
   rootNotionPageId: '1f298309974d80f7a85ec611f8c116d7',
   name: 'Shopify Business Blueprint',
   domain: 'blueprintku.vercel.app', // atau domain kamu sendiri
-  author: 'Nama Kamu',
+  author: 'Prawira Hutagalung',
 
   // Styling, social links, dsb bisa kamu atur di bawahnya
 }
